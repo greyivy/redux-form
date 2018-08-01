e3cec23d7e4dd11d472daf06d5697a41b2099f1b
Will Hauck <will@b-p-s.us>
2018-08-01 12:56:14 -0600

    Allow changing fields to undefined Ignore initial value when changing fields

--- src/createReducer.js
+++ src/createReducer.js
@@ -302,9 +302,9 @@ function createReducer<M, L>(structure: Structure<M, L>) {
     ) {
       let result = state
       const initial = getIn(result, `initial.${field}`)
-      if (initial === undefined && payload === '') {
+      if (payload === '' || payload === undefined) {
         result = deleteInWithCleanUp(result, `values.${field}`)
-      } else if (payload !== undefined) {
+      } else {
         result = setIn(result, `values.${field}`, payload)
       }
       result = deleteInWithCleanUp(result, `asyncErrors.${field}`)
